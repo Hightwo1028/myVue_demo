3038a7fab9af4105dac5c8310323eb1d0e4dbe8a
Hightwo1028 <istoo102800@gmail.com>
2024-07-17 00:28:18 +0800

    0717 myVue_demo

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import Header from './components/header.vue';
+    import Footer from './components/footer.vue';
+</script>
+
+<template>
+    <Header/>
+
+    hightwocode.com
+
+    <Footer/>
+</template>
+
+<style scoped>
+
+</style>
