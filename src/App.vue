7084a4dd6b1804635da0ba4ddccb4f65caa11e66
Hightwo1028 <istoo102800@gmail.com>
2024-07-18 01:13:42 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script setup>
-    import Header from './components/header.vue';
-    import Footer from './components/footer.vue';
+import { reactive } from "vue";
+import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
+
+const propsWeb = {
+  user: 10,
+  url: "www.hightwo.com",
+};
+const userAdd = () => {
+  propsWeb.user++;
+  console.log(propsWeb.user);
+};
 </script>
 
 <template>
-    <Header/>
+  <Header propsName="嗨兔扣頂" propsUrl="hightwo.com" />
 
-    hightwocode.com
+  hightwocode.com
+  <button @click="userAdd">添加事件</button>
 
-    <Footer/>
+  <!-- <Footer v-bind="propsWeb"/> -->D
+  <Footer :="propsWeb" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,9 +1,17 @@
 <script setup>
-
+    const props = defineProps({
+        user: Number,
+        url: {
+            type: String,
+            required: false,
+            default: 'hightwo.com'  //默認值
+        }
+    })
+    console.log(props);
 </script>
 
 <template>
-    <h3>footer</h3>
+    <h3>footer</h3> {{ props.user }}
 </template>
 
 <style scoped>
